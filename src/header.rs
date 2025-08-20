94545a3b467a91d5240e8a3dccf97441a9d2eb85
Coleim <clement.oliva@gmail.com>
2025-08-20 13:43:44 +0200

    Parse header title and logo

--- src/header.rs
+++ src/header.rs
@@ -1,6 +1,6 @@
-pub struct Cartdrige_Header {
+pub struct CartdrigeHeader {
     entry_point: u8,
-    nintendo_logo: [u8; 50],
+    nintendo_logo: [u8; 48],
     title: String,
     manufacturer_code: String,
     publisher: String,
@@ -12,4 +12,74 @@ pub struct Cartdrige_Header {
     version_number: u8,
     header_checksum: u8, // uint8_t checksum = 0; for (uint16_t address = 0x0134; address <= 0x014C; address++) { checksum = checksum - rom[address] - 1; }
     global_checksum: u8,
+}
+
+impl CartdrigeHeader {
+    pub fn new(rom: &[u8]) -> Self {
+        CartdrigeHeader {
+            entry_point: 0,
+            nintendo_logo: Self::read_nintendo_logo(rom),
+            title: Self::parse_title(rom),
+            manufacturer_code: String::new(),
+            publisher: String::new(),
+            cartridge_type: String::new(),
+            rom_size: String::new(),
+            ram_size: String::new(),
+            destination_code: String::new(),
+            old_publisher: String::new(),
+            version_number: 0,
+            header_checksum: 0,
+            global_checksum: 0,
+        }
+    }
+
+    pub fn print(&self) {
+        println!("CartdrigeHeader {{");
+        println!("  entry_point: {}", self.entry_point);
+        println!("  nintendo_logo: {}", self.logo_hex());
+        println!("  title: {}", self.title);
+        println!("  manufacturer_code: {}", self.manufacturer_code);
+        println!("  publisher: {}", self.publisher);
+        println!("  cartridge_type: {}", self.cartridge_type);
+        println!("  rom_size: {}", self.rom_size);
+        println!("  ram_size: {}", self.ram_size);
+        println!("  destination_code: {}", self.destination_code);
+        println!("  old_publisher: {}", self.old_publisher);
+        println!("  version_number: {}", self.version_number);
+        println!("  header_checksum: {}", self.header_checksum);
+        println!("  global_checksum: {}", self.global_checksum);
+        println!("}}");
+    }
+
+    pub fn logo_hex(&self) -> String {
+        self.nintendo_logo
+            .iter()
+            .map(|b| format!("{:02X}", b))
+            .collect::<Vec<String>>()
+            .join(" ")
+    }
+
+    pub fn is_valid(&self) -> Result<(), String> {
+        if self.logo_hex().eq("CE ED 66 66 CC 0D 00 0B 03 73 00 83 00 0C 00 0D 00 08 11 1F 88 89 00 0E DC CC 6E E6 DD DD D9 99 BB BB 67 63 6E 0E EC CC DD DC 99 9F BB B9 33 3E") {
+            println!("Nintendo Logo is valid");
+            Ok(())
+        } else {
+            Err("Nintendo Logo is invalid".to_string())
+        }
+    }
+
+    fn read_nintendo_logo(rom: &[u8]) -> [u8; 48] {
+        let logo_start = 0x0104;
+        let logo_end = 0x0133;
+        let mut logo: [u8; 48] = [0; 48];
+        logo.copy_from_slice(&rom[logo_start..=logo_end]);
+        logo
+    }
+
+    fn parse_title(rom: &[u8]) -> String {
+        let title_start = 0x0134;
+        let title_end = 0x0143;
+        let title_bytes = &rom[title_start..=title_end];
+        String::from_utf8_lossy(title_bytes).to_string()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,16 @@
-mod memory;
-use memory::Memory;
+mod header;
+use header::CartdrigeHeader;
 
-fn main() {
+fn main() -> Result<(), String> {
     // let file_path = "./Tetris.gb";
     let file_path = "./cpu_instrs.gb";
-    let rom = std::fs::read(file_path).unwrap();
-    let memory = Memory::new(&rom);
+    let rom: Vec<u8> = std::fs::read(file_path).unwrap();
+    let header: CartdrigeHeader = CartdrigeHeader::new(&rom);
+    header.print();
 
-    // read header
+    header.is_valid()?;
 
     // create cpu
 
-    let opcode = rom[0];
-    println!("opcode: 0b{:08b} - 0x{:X}", opcode, opcode);
-
-    let logo: [u8; 48] = read_nintendo_logo(&rom);
-    print_logo_ascii(&logo);
-}
-
-fn read_nintendo_logo(rom: &[u8]) -> [u8; 48] {
-    let logo_start = 0x0104;
-    let logo_end = 0x0133;
-    let mut logo: [u8; 48] = [0; 48];
-    logo.copy_from_slice(&rom[logo_start..=logo_end]);
-    logo
-}
-
-fn print_logo_ascii(logo: &[u8; 48]) {
-    for str in logo {
-        print!("{:02X} ", str);
-    }
+    Ok(())
 }
